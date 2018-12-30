6b947f424015c0d96b91792d9cf7af6405639af7
Jun Yuan <mediter@me.com>
2018-12-31 02:10:42 +0800

    finished all 3 of Chapter 3 exercises

---
+++ chapter03-functions-ex.js
@@ -0,0 +1,45 @@
+// Minimum
+
+function min(a, b) {
+  return a <= b ? a : b;
+}
+
+// Recursion
+
+function isEven(number) {
+  if (number < 0) {
+    // with this, it would result in:
+    //   RangeError: Maximum call stack size exceeded
+    return isEven(number * -1);
+  } else if (number === 0) {
+    return true;
+  } else if (number === 1) {
+    return false;
+  } else {
+    return isEven(number - 2);
+  }
+}
+
+
+// Bean Counting
+function countBs(source) {
+  let count = 0;
+  for (var i=0; i < source.length; i++) {
+    if (source[i] === "B") { count += 1 }
+  }
+
+  return count
+}
+
+function countChar(source, targetChar) {
+  let count = 0;
+  for (var i=0; i < source.length; i++) {
+    if (source[i] === targetChar) { count += 1 }
+  }
+
+  return count
+}
+
+function countBsImproved(source) {
+  return countChar(source, "B")
+}
